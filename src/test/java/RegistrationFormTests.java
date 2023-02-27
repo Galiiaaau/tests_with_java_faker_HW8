c9658d80eb9141451726cc2d4ebc4060e52b6a4b
Galiia Uzbekova <galiiauzbekova@gmail.com>
2023-02-27 23:09:37 +0500

    corrected RegistrationFormTests Class name

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistratonFormTests {
+public class RegistrationFormTests {
 
     @BeforeAll
     static void beforeAll() {
